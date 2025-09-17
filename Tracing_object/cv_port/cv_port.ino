7060ec5fd029daddf7cbec84bbfb8bc780a04d47
Solder@Code <alexey.morozov.2023@mail.ru>
2025-09-17 21:40:10 +0300

    Update cv_port.ino
    Signed-off-by: Solder@Code <alexey.morozov.2023@mail.ru>

--- Tracing_object/cv_port/cv_port.ino
+++ Tracing_object/cv_port/cv_port.ino
@@ -1,81 +1,75 @@
-template <typename T, uint8_t N>
+template<typename T, uint8_t N>
 class AverageWindow {
-   public:
-    float filter(T val) {
-        _sum += val;
-        if (++_i >= N) _i = 0;
-        _sum -= _buffer[_i];
-        _buffer[_i] = val;
-        return (float)_sum / N;
-    }
+public:
+  float filter(T val) {
+    _sum += val;
+    if (++_i >= N) _i = 0;
+    _sum -= _buffer[_i];
+    _buffer[_i] = val;
+    return (float)_sum / N;
+  }
 
-   private:
-    T _buffer[N] = {};
-    T _sum = 0;
-    uint8_t _i = 0;
+private:
+  T _buffer[N] = {};
+  T _sum = 0;
+  uint8_t _i = 0;
 };
 AverageWindow<int, 10> aver;
 
 #define ANGLE -600
+#define PINserv 3
+
 
 #include "GyverStepper2.h"
-
+#include "Servo.h"
+Servo ser;
 // Пины: EN=-1 (отключён), DIR=2, STEP=4, MS1=3, MS2=5
 GStepper2<STEPPER4WIRE> stepper(-1, 2, 4, 3, 5);
 
-String receivedData = "";
+String receivedDataX, receivedDataY = "";
 int lastPrinted = 0;
 
 // Фильтр: будем фильтровать само значение X (положение)
-float filteredX = 0;
+float filteredX, FilteredY = 0;
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
-  while (!Serial);
+
+  Servo.attach(PINserv);
 
   stepper.setMaxSpeed(500);
   stepper.setAcceleration(100);
-  stepper.setTargetDeg(0L); // начальное положение
+  stepper.setTargetDeg(0L);  // начальное положение
 }
 
 void loop() {
-  stepper.tick(); // ОБЯЗАТЕЛЬНО
+  stepper.tick();  // ОБЯЗАТЕЛЬНО
 
   // Чтение данных
   while (Serial.available() > 0) {
     char c = Serial.read();
-    if (c == '\n' || c == '\r') {
-      if (receivedData.length() > 0) {
-        int rawX = receivedData.toInt();
+    String receivedDataX = Serial.readStringUntil(':');
+    String receivedDataY = Serial.readStringUntil('\n');
+    int rawX = receivedDataX.toInt();
+    int rawY = receivedDataY.toInt();
+    // === ФИЛЬТРУЕМ САМО ЗНАЧЕНИЕ ===
+    filteredX = expRunningAverageAdaptive(rawX);
+    FilteredY = expRunningAverageAdaptive(rawX);
+    // Устанавливаем ЦЕЛЕВОЕ ПОЛОЖЕНИЕ (можно масштабировать)
 
-        // === ФИЛЬТРУЕМ САМО ЗНАЧЕНИЕ X ===
-        filteredX = expRunningAverageAdaptive(rawX);
-
-        // Устанавливаем ЦЕЛЕВОЕ ПОЛОЖЕНИЕ (можно масштабировать)
-        // Например: 640 пикселей → 180 градусов
-        float targetAngle = map(filteredX, 0, 640, -400, 400);
-        // Ограничим, чтобы не выходить за пределы
-        targetAngle = constrain(targetAngle, -400, 400);
-
-        stepper.setTarget(expRunningAverageAdaptive(aver.filter(targetAngle)));
-
-        // Отправляем ОБРАТНО отфильтрованное значение (или текущее положение)
-        // Для отладки: можно отправить filteredX или (int)stepper.getCurrentDeg()
-        int currentSent = (int)targetAngle;
-        if (abs(currentSent - lastPrinted) > 5) { // только если изменилось существенно
-          Serial.println(currentSent);
-          lastPrinted = currentSent;
-        }
-
-        receivedData = "";
-      }
-    } else {
-      receivedData += c;
-    }
+    float targetAngleX = map(filteredX, 0, 640, -400, 400);
+    float targetAngleY = map(filteredY, 0, 420, 0, 180);
+    // Ограничим, чтобы не выходить за пределы
+    targetAngleX = constrain(targetAngleX, -400, 400);
+    targetAngleY = constrain(targetAngleY, 0, 180);
+    stepper.setTarget(expRunningAverageAdaptive(aver.filter(targetAngleX)));
+    ser.write(expRunningAverageAdaptive(aver.filter(targetAngleY)));
+    // Отправляем ОБРАТНО отфильтрованное значение (или текущее положение)
+    // Для отладки: можно отправить filteredX или (int)stepper.getCurrentDeg()
+    receivedDataX = "";
+    receivedDataY = "";
   }
-
-  //delay(1); // небольшая пауза, чтобы не перегружать
 }
 
 // === Фильтр: адаптивное экспоненциальное сглаживание ===
